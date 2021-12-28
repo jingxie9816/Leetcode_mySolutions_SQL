f6de8bb186f403f515ecc3501f1dacd759c8cb6f
Jing Xie <88932816+jingxie9816@users.noreply.github.com>
2021-12-28 17:06:13 -0600

    Add files via upload

--- 1322.Ads Performance.sql
+++ 1322.Ads Performance.sql
@@ -5,8 +5,8 @@ SELECT ad_id,
          AS DECIMAL(10,2)) AS ctr
 FROM(
 SELECT ad_id,
-       SUM(CASE WHEN action = 'Clicked' THEN 1 ELSE 0 END) as ttl_click,
-       SUM(CASE WHEN action = 'Viewed' THEN 1 ELSE 0 END) as ttl_view
+       SUM(action = 'Clicked') as ttl_click,
+       SUM(action = 'Viewed') as ttl_view
     FROM Ads 
     GROUP BY ad_id) AS T 
 ORDER BY ctr desc, ad_id 
